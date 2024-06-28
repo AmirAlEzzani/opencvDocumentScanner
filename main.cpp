df7ef4263ffc29d57bdc6d0aa66b67c4fdeb5c06
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-06-27 23:52:43 -0700

    ability to screenshot individual frame from webcam feed

--- main.cpp
+++ main.cpp
@@ -1,166 +1,82 @@
 #include <opencv2/opencv.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
 #include <iostream>
+#include <stdio.h>
+#include <windows.h> // For Sleep
+
 
 using namespace cv;
 using namespace std;
 
 
-int main(int argc, char** argv)
+int ct = 0; 
+char screenshot;
+char filename[100]; // For filename
+int  c = 1; // For filename
+
+int main(int, char**)
 {
+
+
+    Mat frame;
+    //--- INITIALIZE VIDEOCAPTURE
     VideoCapture cap;
-    if(!cap.open(0))
-        return 0;
-    for(;;)
-    {
-          Mat frame;
-          cap >> frame;
-          if( frame.empty() ) break;
-          imshow("Cam Footage", frame);
-          if( waitKey(10) == 27 ) break;
+    // open the default camera using default API
+    cap.open(0);
+    // OR advance usage: select any API backend
+    int deviceID = 0;             // 0 = open default camera   
+    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
+                                  // open selected camera using selected API
+    cap.open(deviceID + apiID);
+    // check if we succeeded
+    if (!cap.isOpened()) {
+        cerr << "ERROR! Unable to open camera\n";
+        return -1;
     }
+    //--- GRAB AND WRITE LOOP
+    cout << "Start grabbing" << endl
+        << "Press a to terminate" << endl;
+    for (;;)
+    {
+        // wait for a new frame from camera and store it into 'frame'
+        cap.read(frame);
+
+        if (frame.empty()) {
+            cerr << "ERROR! blank frame grabbed\n";
+            break;
+        }
+
+
+        Sleep(5); // Sleep is mandatory - for no leg!
+
+
+
+        // show live and wait for a key with timeout long enough to show images
+        imshow("CAMERA 1", frame);  // Window name
+
+
+        screenshot = cv::waitKey(30);
+
+
+        if (screenshot == 'q') {
+
+            sprintf_s(filename, "./Frame_%d.jpg", c); // select your folder - filename is "Frame_n"
+            cv::waitKey(10); 
+
+            imshow("CAMERA 1", frame);
+            imwrite(filename, frame);
+            cout << "Frame_" << c << endl;
+            c++;
+        }
+
+
+        if (screenshot == 'a') {
+            cout << "Terminating..." << endl;
+            Sleep(2000);
+            break;
+        }
+
+
+    }
+    // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
-
-void applySepia(cv::Mat &frame)
-{
-  cv::Mat temp = frame.clone();
-  cv::transform(temp, frame, cv::Matx33f(
-    0.272, 0.534, 0.131,
-    0.349, 0.686, 0.168,
-    0.393, 0.769, 0.189)
-  );
-}
-
-void invertColors(cv::Mat &frame)
-{
-  cv::bitwise_not(frame, frame);
-}
-
-void applyGrayscale(cv::Mat &frame)
-{
-  cv::cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
-}
-
-
-
-/* Mat imgOriginal, imgGray, imgBlur, imgCanny, imgThre, imgDil, imgErode, imgWarp, imgCrop;
-vector<Point> initialPoints,docPoints;
-float w = 420, h = 596;
-
-Mat preProcessing(Mat img)
-{
-	cvtColor(img, imgGray, COLOR_BGR2GRAY);
-	GaussianBlur(imgGray, imgBlur, Size(3, 3), 3, 0);
-	Canny(imgBlur, imgCanny, 25, 75);
-	Mat kernel = getStructuringElement(MORPH_RECT, Size(3, 3));
-	dilate(imgCanny, imgDil, kernel);
-	return imgDil;
-}
-
-vector<Point> getContours(Mat image) {
-
-	vector<vector<Point>> contours;
-	vector<Vec4i> hierarchy;
-
-	findContours(image, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-	vector<vector<Point>> conPoly(contours.size());
-	vector<Rect> boundRect(contours.size());
-
-	vector<Point> biggest;
-	int maxArea=0;
-
-	for (int i = 0; i < contours.size(); i++)
-	{
-		int area = contourArea(contours[i]);                                                                                                                              szWA
-
-		string objectType;
-
-		if (area > 1000)
-		{
-			float peri = arcLength(contours[i], true);
-			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
-
-			if (area > maxArea && conPoly[i].size()==4 ) {
-
-				biggest = { conPoly[i][0],conPoly[i][1] ,conPoly[i][2] ,conPoly[i][3] };
-				maxArea = area;
-			}
-		}
-	}
-	return biggest;
-}
-
-void drawPoints(vector<Point> points, Scalar color)
-{
-	for (int i = 0; i < points.size(); i++)
-	{
-		circle(imgOriginal, points[i], 10, color, FILLED);
-		putText(imgOriginal, to_string(i), points[i], FONT_HERSHEY_PLAIN, 4, color, 4);
-	}
-}
-
-vector<Point> reorder(vector<Point> points)
-{
-	vector<Point> newPoints;
-	vector<int>  sumPoints, subPoints;
-
-	for (int i = 0; i < 4; i++)
-	{
-		sumPoints.push_back(points[i].x + points[i].y);
-		subPoints.push_back(points[i].x - points[i].y);
-	}
-
-	newPoints.push_back(points[min_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); // 0
-	newPoints.push_back(points[max_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //1
-	newPoints.push_back(points[min_element(subPoints.begin(), subPoints.end()) - subPoints.begin()]); //2
-	newPoints.push_back(points[max_element(sumPoints.begin(), sumPoints.end()) - sumPoints.begin()]); //3
-
-	return newPoints;
-}
-
-Mat getWarp(Mat img, vector<Point> points, float w, float h )
-{
-	Point2f src[4] = { points[0],points[1],points[2],points[3] };
-	Point2f dst[4] = { {0.0f,0.0f},{w,0.0f},{0.0f,h},{w,h} };
-
-	Mat matrix = getPerspectiveTransform(src, dst);
-	warpPerspective(img, imgWarp, matrix, Point(w, h));
-
-	return imgWarp;
-}
-
-void main() {
-
-	string path = "paper.jpg";
-	imgOriginal = imread(path);
-
-	// Preprpcessing - Step 1 
-	imgThre = preProcessing(imgOriginal);
-
-	// Get Contours - Biggest  - Step 2
-	initialPoints = getContours(imgThre);
-
-	docPoints = reorder(initialPoints);
-
-	// Warp - Step 3 
-	imgWarp = getWarp(imgOriginal, docPoints, w, h);
-
-	//Crop - Step 4
-	int cropVal= 5;
-	Rect roi(cropVal, cropVal, w - (2 * cropVal), h - (2 * cropVal));
-	imgCrop = imgWarp(roi);
-
-	imshow("Image", imgOriginal);
-	imshow("Image Crop", imgCrop);
-	waitKey(0);
-
-	imshow("Image", imgOriginal);
-	imshow("Image Crop", imgCrop);
-	waitKey(0);
-	
-}
-
- */
