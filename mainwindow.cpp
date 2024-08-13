70bc08b9ff019a43038e8ab549a511757bf9be4d
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-08-12 23:50:23 -0700

    added contour outlining. still needs other processing to make text clearer for ocr

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,7 +11,7 @@ MainWindow::MainWindow(QWidget *parent)
     qDebug() << "OpenCV Maj Ver :" << CV_VERSION_MAJOR;
     qDebug() << "OpenCV Min Ver :" << CV_VERSION_MINOR;
 
-    cv::Mat Image = cv::imread("C:/Users/Amir/Documents/qtdocscan/Untitled.png",cv::IMREAD_GRAYSCALE);
+    cv::Mat Image = cv::imread("C:/Users/Amir/Documents/qtdocscan/testimage.jpg",cv::IMREAD_GRAYSCALE);
     cv::Mat Output;
     cv::threshold(Image,Output,100,255,cv::THRESH_BINARY);
     std::vector<std::vector<cv::Point>> contours;
@@ -19,7 +19,7 @@ MainWindow::MainWindow(QWidget *parent)
 
     cv::findContours(Output,contours,hierarchy,cv::RETR_LIST,cv::CHAIN_APPROX_SIMPLE);
     cv::Mat mask = cv::Mat::zeros(Output.size(),CV_8UC3);
-    cv::drawContours(mask,contours,0,cv::Scalar(255,255,0),4);
+    cv::drawContours(mask,contours,-1,cv::Scalar(0,0,255),1);
     std::cout<<contours[0]<<std::endl;
 
 
