3313eadb461612e7189c5332c3e0c71aa616761e
AmirAlEzzani <102375974+AmirAlEzzani@users.noreply.github.com>
2024-09-04 23:18:25 -0700

    added corner detection in preparation for perspective warping

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,8 @@
 #include "QProcess"
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 #include "confirm.h"
 
 MainWindow::MainWindow(QWidget *parent)
@@ -15,14 +17,15 @@ MainWindow::MainWindow(QWidget *parent)
     qDebug() << "OpenCV Maj Ver :" << CV_VERSION_MAJOR;
     qDebug() << "OpenCV Min Ver :" << CV_VERSION_MINOR;
 
-    cv::Mat Image = cv::imread("C:/Users/Amir/Documents/qtdocscan/paper.jpg", cv::IMREAD_GRAYSCALE);
+    cv::Mat input = cv::imread("C:/Users/Amir/Documents/qtdocscan/paper.jpg", cv::IMREAD_GRAYSCALE);
+    cv::Mat Image;
+    cv::resize(input, Image, cv::Size(), 0.75, 0.75);
     cv::Mat Output;
     cv::threshold(Image, Output, 100, 255, cv::THRESH_BINARY);
     std::vector<std::vector<cv::Point>> contours;
     std::vector<cv::Vec4i> hierarchy;
 
     cv::findContours(Output, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
     double maxArea = 0;
     int largestContourIdx = -1;
     for (size_t i = 0; i < contours.size(); i++) {
@@ -30,15 +33,33 @@ MainWindow::MainWindow(QWidget *parent)
         if (area > maxArea) {
             maxArea = area;
             largestContourIdx = i;
-        }
+         }
     }
 
+
+
     cv::Mat mask = cv::Mat::zeros(Output.size(), CV_8UC3);
+    cv::Mat graymask;
     if (largestContourIdx != -1) {
         cv::drawContours(mask, contours, largestContourIdx, cv::Scalar(0, 255, 255), 1);
     }
+    cv::cvtColor(mask, graymask, cv::COLOR_BGR2GRAY);
+
+
+
+    cv::Mat cornersImage = graymask.clone(); // Clone the original image to draw corners on
+    std::vector<cv::Point2f> cornersList;
+    cv::goodFeaturesToTrack(graymask, cornersList, 10, 0.01, 10);
+
+    for (const auto& corner : cornersList) {
+        cv::circle(cornersImage, corner, 5, cv::Scalar(255, 0, 0), -1); // Draw corners
+    }
+
+
     cv::imshow("Input", Image);
-    cv::imshow("Output", mask);
+    cv::imshow("graymask", graymask);
+    cv::imshow("mask", mask);
+    cv::imshow("corners", cornersImage);
     cv::imwrite("output.jpg", mask);
     system("C:/Users/Amir/Documents/qtdocscan/run.bat");
 }
